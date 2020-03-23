bf7f7694129701e25880916ab3c3fc54299965b8
ThomasT <tthai4@gmail.com>
2020-03-22 17:23:20 -0700

    finished

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 //Combined Assignment Code & Event Listener to prompt questions when button pushed
 document.querySelector("#generate").addEventListener("click", writePassword);
 
-// Arrays 
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var special = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", ];
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 
-
-// Variable Declaration 
 var confirmLength = "";
 var confirmSpecialCharacter;
 var confirmNumericCharacter;
@@ -19,7 +16,6 @@ var confirmLowerCase;
 function generatePassword() {
   var confirmLength = (prompt("How many characters would you like in your password?"));
 
-  // Loop if answer is outside the parameters 
   while(confirmLength <=7 || confirmLength >=129) {
       alert("Password length must be between 8-128 characters! Try again");
       var confirmLength = (prompt("How many characters would you like in your password?"));
@@ -27,13 +23,11 @@ function generatePassword() {
 
       alert(`Your password will have ${confirmLength} characters`);
 
-     // Determine parameters of password 
     var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
     var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
     var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
     var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
       
-      // Loop if answer is outside the parameters 
       while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
         alert("You must choose at least one parameter");
         var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
